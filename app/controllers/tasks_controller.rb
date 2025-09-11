294bb0950aab67e659ddd9a973bace88965490e1
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-11 20:48:19 +0800

    Tasks: Added timegate so only dailies would show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.where("created_at <= ?", Time.current.end_of_day)
   end
 
   def show
@@ -17,14 +17,24 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new(task_params)
 
     if @task.save
+      # Assign categories AFTER the task is saved
       if params[:task][:category_ids].present?
-        @task.categories = current_user.categories.where(id: params[:task][:category_ids])
+        category_ids = params[:task][:category_ids].reject(&:blank?)
+        categories = current_user.categories.where(id: category_ids)
+
+        # Clear existing associations first
+        @task.category_tasks.destroy_all
+
+        # Create new associations
+        categories.each do |category|
+          CategoryTask.create!(task: @task, category: category)
+        end
       end
 
       if params[:return_to].present?
-        redirect_to params[:return_to], status: :created
+        redirect_to params[:return_to], notice: "Task was successfully created."
       else
-        redirect_to @task, status: :created
+        redirect_to @task, notice: "Task was successfully created."
       end
     else
       render :new, status: :unprocessable_content
